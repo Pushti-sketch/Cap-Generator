aeb3018c941d124fb54a4deae5b254dd84248532
Pushti-sketch <pushtiuwu@gmail.com>
2025-01-30 18:20:59 +0530

    Update cap-gen.py

--- cap-gen.py
+++ cap-gen.py
@@ -51,30 +51,17 @@ if uploaded_image is not None:
             st.write("Final Meal :")
             st.write(caption)
 footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
 .footer {
 position: fixed;
 left: 0;
 bottom: 0;
 width: 100%;
-background-color: white;
-color: black;
+color: grey;
 text-align: center;
 }
 </style>
 <div class="footer">
-<p>Developed by Jay AKA GameOn <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+<p>Developed by Jay AKA GameOn</p>
 </div>
 """
 st.markdown(footer,unsafe_allow_html=True)
