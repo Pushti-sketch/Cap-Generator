e387a57271130237987a88447c0579933fe3ba75
Pushti-sketch <pushtiuwu@gmail.com>
2025-01-30 18:01:12 +0530

    Update cap-gen.py

--- cap-gen.py
+++ cap-gen.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Initialize the EasyOCR reader (English language)
 reader = easyocr.Reader(['en'])
-
+api_key_1 = st.secrets["google_api_key"]
 # Function to extract text from an image using EasyOCR
 def extract_text_from_image(image):
     result = reader.readtext(image)
@@ -15,7 +15,7 @@ def extract_text_from_image(image):
 # Function to generate caption using Gemini API (via Google SDK)
 def generate_caption_with_gemini(extracted_text):
     # Set up the API key and model using google.generativeai
-    genai.configure(api_key=st.secrets["google_api_key"])  # Replace with your actual API key
+    genai.configure(api_key=api_key_1)  # Replace with your actual API key
     
     model = genai.GenerativeModel("gemini-1.5-flash")
     
