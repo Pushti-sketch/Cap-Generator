366c9c8f5bd6c6df10eb3e4da00fa56076a7ea95
Pushti-sketch <pushtiuwu@gmail.com>
2025-01-30 18:19:33 +0530

    Update cap-gen.py

--- cap-gen.py
+++ cap-gen.py
@@ -50,3 +50,31 @@ if uploaded_image is not None:
             caption = generate_caption_with_gemini(cleaned_text)
             st.write("Final Meal :")
             st.write(caption)
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed by Jay AKA GameOn <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
